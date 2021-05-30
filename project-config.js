244c9ae38f8cc9bc0f692db6348038b5ba3fc810
Kleyton Santos <kleytonbulll@gmail.com>
2021-05-29 23:18:29 -0300

    Adicionar base e estrutura da 404

---
+++ project-config.js
@@ -0,0 +1,5 @@
+const currentProject = "2021_climate_safe"
+
+module.exports ={
+    currentProject
+}
---
+++ src/2021_climate_safe/template/404.php
@@ -0,0 +1,19 @@
+<?php include 'header.php' ?>
+
+<div class="page-wrapper content-wrapper page-404" >
+	<div class="container">
+		<h2>Oops, página não encontrada.</h2>
+		<p>Desculpe, a página que você está procurando não está aqui. Use o campo de pesquisa abaixo para encontrar algo mais ou voltar para <a href="#">Pagina inicial</a> para começar do zero.</p>
+    </div>
+
+    <div class="form__404">
+    	<div class="container">
+    		<form>
+    			<input class="form__404__text" type="text" name="" placeholder="Faça sua busca...">
+    			<input class="form__404__submit" type="submit" name="" value="pesquisar">
+    		</form>
+    	</div>
+    </div>
+</div>
+
+<?php include 'footer.php' ?>
