8fed6ad14cd1c0b74c5e9e5a0e788223d8ac1dc6
Richardson Silva <rafa01sp@hotmail.com>
2021-06-11 10:16:57 -0300

    Adicionar estrutura dos valores, sobre o nosso time

---
+++ src/2021_climate_safe/template/page-about.php
@@ -0,0 +1,66 @@
+<?php include 'header.php' ?>
+
+<div class="page-wrapper content-wrapper">
+    <section class="swiper-container banner">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide" style="background-image: url(assets/images/banner-1.jpg);">
+                <div class="banner__slide">
+                   <h2>titulo do slide um</h2>
+                    <p>texto do slide um</p>
+               </div>
+            </div>
+
+            <div class="swiper-slide" style="background-image: url(assets/images/banner-2.jpg);">
+                <div class="banner__slide">
+                    <h2>titulo do slide dois</h2>
+                    <p>texto do slide dois</p>
+                </div>
+            </div>
+            
+            <div class="swiper-slide" style="background-image: url(assets/images/banner-3.jpg);">
+                <div class="banner__slide">
+                    <h2>titulo do slide três</h2>
+                    <p>texto do slide três</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="numbers">
+        <div class="container">
+            <div class="numbers__item">
+                <h2><b>+30</b> anos</h2>
+                <p>Já desenvolvemos mais de 800 projetos, embasados em experiência e tecnologia, fornecendo soluções inovadoras para um mundo em constante transformação.</p>
+            </div>
+
+            <div class="numbers__item">
+                <h2><b>+30</b> anos</h2>
+                <p>Já desenvolvemos mais de 800 projetos, embasados em experiência e tecnologia, fornecendo soluções inovadoras para um mundo em constante transformação.</p>
+            </div>
+
+            <div class="numbers__item">
+                <h2><b>+30</b> anos</h2>
+                <p>Já desenvolvemos mais de 800 projetos, embasados em experiência e tecnologia, fornecendo soluções inovadoras para um mundo em constante transformação.</p>
+            </div>
+
+            <div class="numbers__item">
+                <h2><b>+30</b> anos</h2>
+                <p>Já desenvolvemos mais de 800 projetos, embasados em experiência e tecnologia, fornecendo soluções inovadoras para um mundo em constante transformação.</p>
+            </div>
+        </div>
+    </section>
+    
+    <section class="solution">
+        <hr>
+        <div class="container">
+            <h2>Nossas Soluções</h2>
+            <p>A WayCarbon possui serviços e produtos integrados que promovem a gestão da sustentabilidade em iniciativas públicas e privadas. Conheça!</p>
+        </div>
+    </section>
+    <section  class="sign up" >
+            <h2>Quer ficar por dentro de nóticias e conteúdos da nossa newsletter?</h2>
+    </section>
+    
+</div>
+
+<?php include 'footer.php' ?>
