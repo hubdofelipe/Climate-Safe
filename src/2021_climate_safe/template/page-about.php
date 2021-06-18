4c2680e8c1acc9b9e9b5bde754232f2129ca4f52
Richardson Silva <rafa01sp@hotmail.com>
2021-06-18 14:33:00 -0300

    Adicionar estrutura de valores, time, selos

--- src/2021_climate_safe/template/page-about.php
+++ src/2021_climate_safe/template/page-about.php
@@ -1,64 +1,58 @@
 <?php include 'header.php' ?>
 
 <div class="page-wrapper content-wrapper">
-    <section class="swiper-container banner">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide" style="background-image: url(assets/images/banner-1.jpg);">
-                <div class="banner__slide">
-                   <h2>titulo do slide um</h2>
-                    <p>texto do slide um</p>
-               </div>
-            </div>
-
-            <div class="swiper-slide" style="background-image: url(assets/images/banner-2.jpg);">
-                <div class="banner__slide">
-                    <h2>titulo do slide dois</h2>
-                    <p>texto do slide dois</p>
-                </div>
-            </div>
-            
-            <div class="swiper-slide" style="background-image: url(assets/images/banner-3.jpg);">
-                <div class="banner__slide">
-                    <h2>titulo do slide três</h2>
-                    <p>texto do slide três</p>
-                </div>
-            </div>
+<hr>
+    <section class="page-about">
+        <div class="page-about__text"> 
+            <h2>Sustentabilidade e tecnologia para um mundo em transformação.</h2>
+            <h3>Com 14 anos de mercado, a WayCarbon é considerada referência nacional em consultoria e no desenvolvimento de soluções de tecnologia e inovação voltadas para a sustentabilidade, gestão de ativos ambientais e no desenvolvimento de estratégias visando a ecoeficiência e a economia de baixo carbono.</h3>
+            <p>Já concluímos mais de 800 projetos em cerca de 200 das maiores empresas brasileiras dos setores público e privado. Além disso, assessoramos diversos municípios, estados e o Governo Federal na temática da mudança do clima.</p> 
+        </div>
+        <div class="page-about__imagem">
+            <img src="assets/images/banner-4.jpg" alt="">
         </div>
     </section>
     
-    <section class="numbers">
+    <section class="values">
+        <div class="values__text">
+            <h2>Nossos Valores</h2>
+            <p>Conheça os valores que conduzem e motivam o nosso trabalho.</p>
+        </div>
         <div class="container">
-            <div class="numbers__item">
-                <h2><b>+30</b> anos</h2>
-                <p>Já desenvolvemos mais de 800 projetos, embasados em experiência e tecnologia, fornecendo soluções inovadoras para um mundo em constante transformação.</p>
+    
+            <div class="values__item">
+                <h2><b><i class="far fa-lightbulb"></i></b> MISSÃO</h2>
+                <p>Empregamos conhecimento e tecnologia para apoiar nossos clientes a superar os desafios de um mundo em rápida transformação.</p>
             </div>
 
-            <div class="numbers__item">
-                <h2><b>+30</b> anos</h2>
-                <p>Já desenvolvemos mais de 800 projetos, embasados em experiência e tecnologia, fornecendo soluções inovadoras para um mundo em constante transformação.</p>
+            <div class="values__item">
+                <h2><b><i class="fab fa-bitbucket"></i></b> VISÃO</h2>
+                <p>Ser a empresa de base tecnológica referência em sustentabilidade e contribuir para a construção de um mundo melhor</p>
             </div>
 
-            <div class="numbers__item">
-                <h2><b>+30</b> anos</h2>
-                <p>Já desenvolvemos mais de 800 projetos, embasados em experiência e tecnologia, fornecendo soluções inovadoras para um mundo em constante transformação.</p>
+            <div class="values__item">
+                <h2><b><i class="fas fa-rocket"></i></b> VALORES</h2>
+                <p>Trabalhamos para impactar a sociedade positivamente, pautados pela inovação, pela valorização das pessoas, pela geração de valor e pela ética.</p>
             </div>
 
-            <div class="numbers__item">
-                <h2><b>+30</b> anos</h2>
-                <p>Já desenvolvemos mais de 800 projetos, embasados em experiência e tecnologia, fornecendo soluções inovadoras para um mundo em constante transformação.</p>
-            </div>
         </div>
     </section>
     
-    <section class="solution">
-        <hr>
+    <section class="team">
         <div class="container">
-            <h2>Nossas Soluções</h2>
-            <p>A WayCarbon possui serviços e produtos integrados que promovem a gestão da sustentabilidade em iniciativas públicas e privadas. Conheça!</p>
+            <h2>O nosso time</h2>
+            <p>om uma equipe qualificada e multidisciplinar, a WayCarbon fornece ao mercado uma proposta de valor incomparável, alinhando o conhecimento e a expertise da consultoria em sustentabilidade com o desenvolvimento de produtos inovadores.</p>
         </div>
     </section>
-    <section  class="sign up" >
-            <h2>Quer ficar por dentro de nóticias e conteúdos da nossa newsletter?</h2>
+    <section  class="seals" >
+        <h2>NOSSOS SELOS, CERTIFICAÇÕES E FILIAÇÕES</h2>
+        <div class="container">
+            <img src="assets/images/img-1.png" alt="">
+            <img src="assets/images/img-1.png" alt="">
+            <img src="assets/images/img-1.png" alt="">
+            <img src="assets/images/img-1.png" alt="">
+            <img src="assets/images/img-1.png" alt="">
+            </div>
     </section>
     
 </div>
